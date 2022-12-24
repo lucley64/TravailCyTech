8d97ca5a61a75a30138f727d55e45bd13d92f4c1
lucley64 <luc.leydert@gmail.com>
2022-12-24 18:54:41 +0100

    comm sur jeu.h

--- S1/ProgramationProc/TP9-blackjack/src/jeu.h
+++ S1/ProgramationProc/TP9-blackjack/src/jeu.h
@@ -16,10 +16,17 @@
 
 /// @brief Structure contenant tout le jeu
 typedef struct {
+    /// @brief La liste des joueurs
     listJoueur listJoueur;
+    /// @brief La liste des cartes
     ListCarte cartes;
 } Jeu;
 
+/// @brief Initialise la partie
+/// @param jeu Pointeur sur le jeu
+/// @param nbJoueurs Le nombre de joueurs
 void initialiserJeu(Jeu* jeu, int nbJoueurs);
 
+/// @brief Boucle de jeu principale
+/// @param jeu Pointeur sur le jeu
 void gameLoop(Jeu* jeu);
