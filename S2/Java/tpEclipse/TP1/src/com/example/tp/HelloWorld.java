045aa6f2346033ff8402fd18203f3997e1e89127
lucley64 <luc.leydert@gmail.com>
2023-01-19 10:56:16 +0100

    ah

--- S2/Java/tpEclipse/TP1/src/com/example/tp/HelloWorld.java
+++ S2/Java/tpEclipse/TP1/src/com/example/tp/HelloWorld.java
@@ -1,9 +1,8 @@
-
+package com.example.tp;
+import java.lang.System.Logger.Level;
 public class HelloWorld {
-
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
-
+		var logger = System.getLogger("mainLogger");
+		logger.log(Level.INFO, "Hello, World!");
 	}
-
 }
--- S2/Java/tpVscode/TP1/src/main/java/com/example/tp/App.java
+++ S2/Java/tpVscode/TP1/src/main/java/com/example/tp/App.java
@@ -1,6 +1,10 @@
 package com.example.tp;
+
+import java.lang.System.Logger.Level;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+		var logger = System.getLogger("mainLogger");
+		logger.log(Level.INFO, "Hello, World!");
     }
 }
