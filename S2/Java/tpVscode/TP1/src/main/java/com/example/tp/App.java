5bbdebf0622db8b7612670edb1e9365bff695ba2
lucley64 <luc.leydert@gmail.com>
2023-01-19 10:44:46 +0100

    tp1 Java complete

--- S2/Java/tpVscode/TP1/src/main/java/com/example/tp/App.java
+++ S2/Java/tpVscode/TP1/src/main/java/com/example/tp/App.java
@@ -1,5 +1,6 @@
+package com.example.tp;
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
 }
