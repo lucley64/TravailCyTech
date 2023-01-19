643c29569f017e11273d31c1cddcf31876f4cc3b
lucley64 <luc.leydert@gmail.com>
2023-01-19 11:18:20 +0100

    fix build

--- S2/Java/tpEclipse/TP1/src/com/example/tp/App.java
+++ S2/Java/tpEclipse/TP1/src/com/example/tp/App.java
@@ -1,6 +1,6 @@
 package com.example.tp;
 import java.lang.System.Logger.Level;
-public class HelloWorld {
+public class App {
 	public static void main(String[] args) {
 		var logger = System.getLogger("mainLogger");
 		logger.log(Level.INFO, "Hello, World!");
