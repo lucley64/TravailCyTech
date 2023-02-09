42abd1e56711197ae7a39aeac6f6745a71757ff5
lucley64 <luc.leydert@gmail.com>
2023-02-09 09:38:13 +0100

    TP3

--- S2/Java/tpVscode/TP3/src/main/java/com/example/tp/Point.java
+++ S2/Java/tpVscode/TP3/src/main/java/com/example/tp/Point.java
@@ -3,62 +3,71 @@ package com.example.tp;
 import java.lang.System.Logger.Level;
 
 public class Point {
-    private int x;
-    private int y;
+    private double r;
+    private double theta;
+    private double x;
+    private double y;
 
     public Point() {
         this(0, 0);
     }
 
-    public Point(int x, int y) {
-        setX(x);
-        setY(y);
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public void setX(int x) {
+    public Point(double x, double y) {
         this.x = x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setY(int y) {
         this.y = y;
+        transform();
+    }
+
+    public double getX() {
+        return Math.cos(theta) * r;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+        transform();
+    }
+
+    public double getY() {
+        return (Math.cos(-theta)) * r;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+        transform();
+    }
+
+    private void transform(){
+        r = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+        theta = (r == 0) ? 0 : Math.acos(x / r);
     }
 
     @Override
     public String toString() {
-        return "(" + x + ", " + y + ")";
+        return "(" + r + ", " + theta + ")";
     }
 
     @Override
     public boolean equals(Object obj) {
-        if (obj instanceof Point){
+        if (obj instanceof Point) {
             var p = (Point) obj;
-            return p.x == x && p.y == y;
+            return p.r == r && p.theta == theta;
         }
         return false;
     }
 
     @Override
     public int hashCode() {
-        return x * y;
+        return (int) (getX() * getY());
     }
 
-    public int distance(Point p){
-        return (Math.abs(x - p.x) * Math.abs(y - p.y)) / 2 ;
+    public double distance(Point p) {
+        return (Math.abs(getX() - p.getX()) * Math.abs(getY() - p.getY())) / 2;
     }
 
-    public void translater(Point p){
-        setX(x + p.x);
-        setY(y + p.y);
+    public void translater(Point p) {
+        setX(getX() + p.getX());
+        setY(getY() + p.getY());
     }
-
 
     public static void main(String[] args) {
         var p1 = new Point();
@@ -67,8 +76,10 @@ public class Point {
         var logger = System.getLogger("mainLogger");
 
         logger.log(Level.INFO, "Distance entre p1 et p2 : " + p1.distance(p2));
-
+        
         p1.translater(p2);
+        logger.log(Level.INFO, "p2 : " + p2.getX() + ", " + p2.getY());
+        logger.log(Level.INFO, "p1 : " + p1.getX() + ", " + p1.getY());
 
         logger.log(Level.INFO, "p1.equals(p2) ? " + p1.equals(p2) + "\n\t p1 == p2 ? " + (p1 == p2));
 
