5b2979ebb95372e4c48b5649ee769db01509a071
Zelie <luc.leydert@gmail.com>
2023-03-15 11:07:38 +0100

    des choses

--- S2/DevWeb/TPs/js/main.js
+++ S2/DevWeb/TPs/js/main.js
@@ -2,3 +2,11 @@ function resizeIFrameToFitContent(iFrame) {
     if (iFrame.contentWindow)
         iFrame.height = iFrame.contentWindow.document.body.scrollHeight + 50 + "px";
 }
+function changeUrl(ev, item) {
+    ev.preventDefault();
+    var iFrame = document.querySelector("#main-frame");
+    iFrame.src = item.href;
+}
+window.onload = function () {
+    document.querySelector("#lien-accueil").click();
+};
--- S2/DevWeb/TPs/ts/main.ts
+++ S2/DevWeb/TPs/ts/main.ts
@@ -2,3 +2,14 @@ function resizeIFrameToFitContent(iFrame: HTMLIFrameElement) {
     if (iFrame.contentWindow)
         iFrame.height = iFrame.contentWindow.document.body.scrollHeight + 50 + "px";
 }
+
+function changeUrl(ev: MouseEvent, item: HTMLAnchorElement){
+    ev.preventDefault();
+    const iFrame: HTMLIFrameElement = document.querySelector("#main-frame") as HTMLIFrameElement;
+    iFrame.src = item.href;
+    
+}
+
+window.onload = () => {
+    (document.querySelector("#lien-accueil") as HTMLAnchorElement).click();
+}
