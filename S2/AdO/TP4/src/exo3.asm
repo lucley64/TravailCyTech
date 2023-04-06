e9becc21acdb07f9482c5349dda7c64c7b8d1ae8
lucley64 <luc.leydert@gmail.com>
2023-04-06 11:33:43 +0200

    anal 2

--- S2/AdO/TP4/src/exo3.asm
+++ S2/AdO/TP4/src/exo3.asm
@@ -5,14 +5,27 @@ section .data
     prompt2 db 'Entrer b : '
     lenprompt2 equ $ - prompt2
 
-    resu db 'a + b = '
-    lenresu equ $ - resu
+    resadd db 'a + b = '
+    lenresadd equ $ - resadd
 
-    nl db ,13,10
+    ressub db 'a - b = '
+    lenressub equ $ - ressub
+
+    resmul db 'a × b = '
+    lenresmul equ $ - resmul
+
+    resdiv db 'a ÷ b = '
+    lenresdiv equ $ - resdiv
+
+    nl db 0xA
     lennl equ $ - nl
 
+
 section .bss
-    inp resb 5
+    inp1 resb 10
+    inp2 resb 10
+    res resb 10
+    resres resb 10
 
 
 section .text
@@ -27,11 +40,153 @@ _start:
 
     mov eax, 3
     mov ebx, 0
-    mov ecx, inp
-    mov edx, 5
+    mov ecx, inp1
+    mov edx, 10
+    int 0x80
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, prompt2
+    mov edx, lenprompt2
+    int 0x80
+
+    mov eax, 3
+    mov ebx, 0
+    mov ecx, inp2
+    mov edx, 10
+    int 0x80
 
 
+    mov eax, [inp1]
+    sub eax, '0'
+    mov ebx, [inp2]
+    sub ebx, '0'
 
+    add eax, ebx
+
+    add eax, 0x30
+    
+    mov [res], eax
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, resadd
+    mov edx, lenresadd
+    int 0x80
+
+    printAdd:
+        mov eax, [res]
+        sub eax, 0x30
+        mov ebx, 0xA
+        div ebx
+        add eax, 0x30
+        mov [res], eax
+        mov [resres], eax
+
+        mov eax, 4
+        mov ebx, 1
+        mov ecx, res
+        mov edx, 10
+        int 0x80
+
+
+        mov eax, [resres]
+        mov [res], eax
+        cmp eax, 0x0
+        jne printAdd
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, nl
+    mov edx, lennl
+    int 0x80
+
+    mov eax, [inp1]
+    sub eax, '0'
+    mov ebx, [inp2]
+    sub ebx, '0'
+
+    sub eax, ebx
+
+    add eax, 0x30
+    
+    mov [res], eax
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, ressub
+    mov edx, lenressub
+    int 0x80
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, res
+    mov edx, 1
+    int 0x80
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, nl
+    mov edx, lennl
+    int 0x80
+
+    mov eax, [inp1]
+    sub eax, '0'
+    mov ebx, [inp2]
+    sub ebx, '0'
+
+    mul ebx
+
+    add eax, 0x30
+    
+    mov [res], eax
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, resmul
+    mov edx, lenresmul
+    int 0x80
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, res
+    mov edx, 1
+    int 0x80
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, nl
+    mov edx, lennl
+    int 0x80
+
+    mov eax, [inp1]
+    sub eax, '0'
+    mov ebx, [inp2]
+    sub ebx, '0'
+
+    div ebx
+
+    add eax, 0x30
+    
+    mov [res], eax
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, resdiv
+    mov edx, lenresdiv
+    int 0x80
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, res
+    mov edx, 1
+    int 0x80
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, nl
+    mov edx, lennl
+    int 0x80
 
     mov eax, 1      ;system call number
     mov ebx, 0      ;   0 = exit code
