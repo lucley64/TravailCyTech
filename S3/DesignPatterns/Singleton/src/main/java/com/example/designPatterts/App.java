ae2d148350966d2ce438a8c40c8bdefe397b3549
lucley64 <luc.leydert@gmail.com>
2023-09-12 11:37:39 +0200

    aaaaaaaa

---
+++ S3/DesignPatterns/Singleton/src/main/java/com/example/designPatterts/App.java
@@ -0,0 +1,11 @@
+package com.example.ihm;
+
+import com.example.ihm.presentation.Visionneuse;
+
+import javafx.application.Application;
+
+public class App {
+    public static void main(String[] args) {
+        Application.launch(Visionneuse.class, args);
+    }
+}
