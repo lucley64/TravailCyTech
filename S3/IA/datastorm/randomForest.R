659434f358ae7ba9ceb0d76664b21c1fcaf71694
lucley64 <luc.leydert@gmail.com>
2023-09-27 10:03:49 +0200

    gné aiiiiiiiiii

--- S3/IA/datastorm/randomForest.R
+++ S3/IA/datastorm/randomForest.R
@@ -8,3 +8,15 @@ model$oob.times
 
 model$importance
 varImpPlot(model)
+
+newIrirs <- as.data.frame(t(c(4, 3, 1, 2)))
+names(newIrirs) <- names(iris)[1:4]
+predict(model, newIrirs)
+predict(model, newIrirs, type = "prob")
+
+forest1 <- randomForest(Species ~ ., iris, ntree = 100)
+forest2 <- randomForest(Species ~ ., iris, ntree = 500)
+model <- combine(forest1, forest2)
+
+plot(forest2$err.rate[, 1], type = "l")
+
