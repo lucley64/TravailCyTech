4cdd7b39082e477d1762212ca3d1af4a70e9a145
lucley64 <luc.leydert@gmail.com>
2023-10-26 10:12:47 +0200

    state strategy

--- S3/DesignPatterns/StateStrategy/src/main/java/com/example/designpatterns/state/KeypadState.java
+++ S3/DesignPatterns/StateStrategy/src/main/java/com/example/designpatterns/state/KeypadState.java
@@ -3,5 +3,5 @@ package com.example.designpatterns.state;
 public interface KeypadState {
 
 
-    public abstract boolean keyPressed(Keypad keypad, char c);
+    boolean keyPressed(Keypad keypad, char c);
 }
