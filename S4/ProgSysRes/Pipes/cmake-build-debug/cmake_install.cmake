46e295f10892de0fb9e5cf581a0279294328c5d1
lucley64 <luc.leydert@gmail.com>
2024-02-13 09:56:07 +0100

    prog sys res

--- S4/ProgSysRes/Pipes/cmake-build-debug/cmake_install.cmake
+++ S4/ProgSysRes/Pipes/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/cytech/Travail/S4/ProgPara/Pipes
+# Install script for directory: /home/cytech/Travail/S4/ProgSysRes/Pipes
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/cytech/Travail/S4/ProgPara/Pipes/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/cytech/Travail/S4/ProgSysRes/Pipes/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
